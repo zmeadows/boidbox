3d0c1df500e0a937cacc85f2562440f6ef8d671f
Zac Meadows <zmeadows@gmail.com>
2024-09-21 10:57:08 -0700

    initial commit

---
+++ boid.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void testfn() {
+	printf("testfn\n");
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "boid.h"
+
+int main(/* int argc, char** argv */)
+{
+	printf("hello world!\n");
+	testfn();
+	return 0;
+}
+
